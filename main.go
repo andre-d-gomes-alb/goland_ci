ef2534b97a5d1b69339983b50b58d6b9054a4ff3
ANDRE FILIPE DOMINGOS GOMES <andre-d-gomes@telecom.pt>
2021-08-04 15:03:27 +0100

    Update app

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
 }
 
 func Router() *mux.Router {
